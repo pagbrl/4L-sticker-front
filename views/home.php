acbf4a2023d53621ddf60a59c0e6dba8fd1e74dd
Théo KLEMAN <theo@kleman.fr>
2016-02-01 18:03:02 +0100

    WIP home page

---
+++ views/home.php
@@ -0,0 +1,15 @@
+<section class="home-template">
+    <div class="content--left">
+        cc
+    </div>
+    <div class="content--right">
+        <header>
+            <h2>La journée d'hier</h2>
+            <span class="hashtag">#MPGAZL</span>
+        </header>
+        <div class="instagram-flux">
+            
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
