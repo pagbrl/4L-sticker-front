fcb2f29e0b5426d441fec4748f2867e9812dbd0c
Théo KLEMAN <theo@kleman.fr>
2016-02-01 18:18:58 +0100

    WIP home instagram flux

--- views/home.php
+++ views/home.php
@@ -1,6 +1,6 @@
 <section class="home-template">
     <div class="content--left">
-        cc
+        
     </div>
     <div class="content--right">
         <header>
@@ -8,7 +8,43 @@
             <span class="hashtag">#MPGAZL</span>
         </header>
         <div class="instagram-flux">
-            
+            <article class="large">
+                <img src="http://lorempixel.com/560/560" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <div class="clear"></div>
+        </div>
+        <header>
+            <h2>Lundi 12</h2>
+        </header>
+        <div class="instagram-flux">
+            <article class="large">
+                <img src="http://lorempixel.com/560/560" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <article class="mini">
+                <img src="http://lorempixel.com/260/260" alt="">
+            </article>
+            <div class="clear"></div>
         </div>
     </div>
     <div class="clear"></div>
