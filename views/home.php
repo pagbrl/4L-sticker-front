af0edf4e883faf25b034630a51c302e35d072d64
Théo KLEMAN <theo@kleman.fr>
2016-02-01 18:48:26 +0100

    Right content

--- views/home.php
+++ views/home.php
@@ -3,48 +3,69 @@
         
     </div>
     <div class="content--right">
-        <header>
-            <h2>La journée d'hier</h2>
-            <span class="hashtag">#MPGAZL</span>
-        </header>
-        <div class="instagram-flux">
-            <article class="large">
-                <img src="http://lorempixel.com/560/560" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <div class="clear"></div>
+        <div class="right-scrollable-zone">
+            <header>
+                <h2>La journée d'hier</h2>
+                <span class="hashtag">#MPGAZL</span>
+            </header>
+            <div class="instagram-flux">
+                <article class="large">
+                    <img src="http://lorempixel.com/560/560" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <div class="clear"></div>
+            </div>
+            <header>
+                <h2>Lundi 12</h2>
+            </header>
+            <div class="instagram-flux">
+                <article class="large">
+                    <img src="http://lorempixel.com/560/560" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <article class="mini">
+                    <img src="http://lorempixel.com/260/260" alt="">
+                </article>
+                <div class="clear"></div>
+            </div>
         </div>
-        <header>
-            <h2>Lundi 12</h2>
-        </header>
-        <div class="instagram-flux">
-            <article class="large">
-                <img src="http://lorempixel.com/560/560" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <article class="mini">
-                <img src="http://lorempixel.com/260/260" alt="">
-            </article>
-            <div class="clear"></div>
+        <div class="is-scrollable">
+            <span>
+                <svg version="1.1"
+                     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+                     x="0px" y="0px" width="6.4px" height="35.4px" viewBox="0 0 6.4 35.4" style="enable-background:new 0 0 6.4 35.4;"
+                     xml:space="preserve">
+                    <style type="text/css">
+                        .st0{fill:#FFC600;}
+                    </style>
+                    <defs>
+                    </defs>
+                    <circle class="st0" cx="3.2" cy="3.2" r="3.2"/>
+                    <circle class="st0" cx="3.2" cy="18.2" r="3.2"/>
+                    <circle class="st0" cx="3.2" cy="32.2" r="3.2"/>
+                </svg>
+
+                <!-- <p>Voir les jours précédents</p> -->
+            </span>
         </div>
     </div>
     <div class="clear"></div>
