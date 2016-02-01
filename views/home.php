6b9c63b7f5af8f83306910782666d8ce4f0e83b8
Praszezinski William <praszezinskiw@gmail.com>
2016-02-01 23:39:15 +0100

    Home css
    Manque l’intégration d’instagram 😭

--- views/home.php
+++ views/home.php
@@ -1,6 +1,21 @@
 <section class="home-template">
     <div class="content--left">
-        
+      <div class="left--container">
+        <div class="vCenter">
+          <img src="http://lorempixel.com/300/300">
+            <div class="left--info">
+            <h1>Soutenez l’association Enfants du Désert et participez à l’aventure du 4L Trophy</h1>
+            <p>Depuis 11 ans, l’association Enfants du Désert organise le 4L Trophy : tous les ans, c’est plus de 1500 équipages qui sillonnent à bord de leur 4L Xkm de routes et de pistes à travers l’Europe.<br />
+  Le 18 Février, Pauline & Margaux sont parties le coffre rempli de fournitures scolaire qu’elles distribueront aux enfants marocains pendant 10 jours. Prenez part à leur aventure !</p>
+
+            <p>Pour participer, deux solutions : créez votre 4L personnalisée et affichez-la accompagnée d’un petit message sur la voiture de l’équipage. Vous pouvez également soutenir directement l’association en faisant un don.</p>
+
+            <a href="#" class="btn">Créer mon sticker</a>
+            <a href="#">Faire un don à enfant du désert</a>
+
+          </div>
+        </div>
+      </div>
     </div>
     <div class="content--right">
         <div class="right-scrollable-zone">
