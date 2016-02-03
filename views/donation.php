d9e6f418160e1dec85bf99faf6cc3dcfc325a343
Praszezinski William <praszezinskiw@gmail.com>
2016-02-03 16:03:14 +0100

    Page don 👌

--- views/donation.php
+++ views/donation.php
@@ -1,1 +1,39 @@
-<p>allez la putain</p>
+<section class="don-template">
+  <div class="don-go-home">
+  <a href="./">
+    <svg width="30px" height="30px" viewBox="0 0 25 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+    <!-- Generator: Sketch 3.4.4 (17249) - http://www.bohemiancoding.com/sketch -->
+      <title>Rectangle 34</title>
+      <desc>Created with Sketch.</desc>
+      <defs></defs>
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+          <g id="Don-sans-sticker" sketch:type="MSArtboardGroup" transform="translate(-29.000000, -26.000000)" fill="#FFC600">
+              <g id="Cross" sketch:type="MSLayerGroup" transform="translate(41.213203, 38.213203) rotate(-315.000000) translate(-41.213203, -38.213203) translate(26.213203, 23.213203)">
+                  <path d="M17.1428571,12.8571429 L17.1428571,4.26325641e-14 L12.8571429,4.26325641e-14 L12.8571429,12.8571429 L4.26325641e-14,12.8571429 L4.26325641e-14,17.1428571 L12.8571429,17.1428571 L12.8571429,30 L17.1428571,30 L17.1428571,17.1428571 L30,17.1428571 L30,12.8571429 L17.1428571,12.8571429 Z" id="Rectangle-34" sketch:type="MSShapeGroup"></path>
+              </g>
+          </g>
+      </g>
+    </svg>
+    <span>Revenir à l'accueil</span>
+  </a>
+  </div>
+  <div class="don-container">
+    <div>
+      <h2>Faire un don à Enfants du Désert</h2>
+      <p>
+        L’association <a href="#">Enfants du désert</a> agi depuis 2005 pour faciliter l’accès à l’éducation des enfants du sud Marocain.<br />
+  Votre don permettra de financer divers projets comme la construction d’écoles ou l’amélioration des conditions médicales des jeunes marocains.
+      </p>
+      <div class="don-input-btn">
+        <div class="panel-cta">
+          <div class="amount-input">
+            <label for="amount">Montant</label>
+            <input type="number" id="amount" min="0" placeholder="3,00">
+            <span>€</span>
+          </div>
+        </div>
+        <a href="#" class="btn">Faire un don</a>
+      </div>
+    </div>
+  </div>
+</section>
